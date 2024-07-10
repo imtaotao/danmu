040d25cbc9c2f603c6bcd04e2b285732b12738a7
imtaotao <dwchentao@163.com>
2024-07-10 18:18:52 +0800

    feat: add github log

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import type { Manager } from 'danmu';
 import type { BarrageValue } from '@/types';
+import githubLogo from '@/assets/github.svg';
 import { Area } from '@/components/custom/area';
 import { Toaster } from '@/components/ui/toaster';
 import { Sidebar } from '@/components/custom/sidebar';
@@ -35,6 +36,13 @@ export function App({ manager }: { manager: Manager<BarrageValue> }) {
     <div className="w-full bg-slate-200">
       <div className="mx-auto aspect-[4/2] p-3 h-screen flex text-slate-600">
         <div className="min-w-96 w-96 mr-2 px-4 py-3 border-slate-400 border-indigo-500/50 rounded-sm bg-slate-300">
+          <a target="_blank" href="https://github.com/imtaotao/danmuku">
+            <img
+              src={githubLogo}
+              alt="github logo"
+              className="w-[30px] h-[30px] mb-4"
+            />
+          </a>
           <Sidebar
             manager={manager}
             allNumber={allNumber}
