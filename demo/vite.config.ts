7edbe0a04bcb604cc51ade5a495368dd99612114
imtaotao <dwchentao@163.com>
2024-07-10 18:56:19 +0800

    chore: adjust workflow and vite.config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'output',
-  build: {
-    outDir: '../output',
-  },
+  base: 'danmuku',
   plugins: [react()],
   resolve: {
     alias: {
