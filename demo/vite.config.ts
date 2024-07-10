14c253f4eb812292b222dac39f0e1f0c8593e13a
imtaotao <dwchentao@163.com>
2024-07-10 18:36:26 +0800

    chore: adjust vite build config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'output',
+  build: {
+    outDir: '../output',
+  },
   plugins: [react()],
-  build: { outDir: '../output' },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
