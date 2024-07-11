47e4103c80123e152bdc7a291729b9f70b246a58
imtaotao <dwchentao@163.com>
2024-07-11 09:53:52 +0800

    feat: adjust demo

--- demo/src/components/sidebar/sidebarModeSelect.tsx
+++ demo/src/components/sidebar/sidebarModeSelect.tsx
@@ -26,6 +26,14 @@ export const SidebarModeSelect = memo(
               </TooltipTrigger>
               <TooltipContent>
                 渲染模式决定着碰撞检测的规则和弹幕渲染的时机
+                <br />
+                1. 当设置为 none 模式时，不会有任何碰撞检测，弹幕会立即渲染
+                <br />
+                2. 当设置为 strict
+                模式时，会进行严格的碰撞检测，如果不满足条件则会推迟渲染
+                <br />
+                3. 当设置为 adaptive
+                模式时，在满足立即渲染的前提下，会尽力进行碰撞检测（推荐）
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
@@ -33,47 +41,20 @@ export const SidebarModeSelect = memo(
         <Tabs
           defaultValue="strict"
           onFocus={(e) =>
-            manager.updateOptions({ mode: e.target.textContent as Mode })
+            manager.updateOptions({
+              mode: e.target.textContent?.trim() as Mode,
+            })
           }
         >
           <TabsList>
             <TabsTrigger className="px-2 font-bold" value="none">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <div>none</div>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    当设置为 none 模式时，不会有任何碰撞检测，弹幕会立即渲染
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+              none
             </TabsTrigger>
             <TabsTrigger className="px-2 font-bold" value="strict">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <div>strict</div>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    当设置为 strict
-                    模式时，会进行严格的碰撞检测，如果不满足条件则会推迟渲染
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+              strict
             </TabsTrigger>
             <TabsTrigger className="px-2 font-bold" value="adaptive">
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <div>adaptive</div>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    当设置为 adaptive
-                    模式时，在满足立即渲染的前提下，会尽力进行碰撞检测（推荐）
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+              adaptive
             </TabsTrigger>
           </TabsList>
         </Tabs>
