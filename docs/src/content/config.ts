c7fb3e91215462b938dd94d36f7e2878a32418b4
imtaotao <chentao.arthur@bytedance.com>
2024-08-14 11:06:57 +0800

    fix: fix docs build error

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,10 +1,6 @@
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { i18nSchema } from '@astrojs/starlight/schema';
 import { defineCollection } from 'astro:content';
-import { blogSchema } from 'starlight-blog/schema';
 
 export const collections = {
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-  docs: defineCollection({
-    schema: docsSchema({ extend: (ctx) => blogSchema(ctx) }),
-  }),
 };
