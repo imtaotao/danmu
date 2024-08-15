0d5689ff948913e9c2ffa614240c3003224de65b
imtaotao <dwchentao@163.com>
2024-08-16 00:36:11 +0800

    feat: add cd snippet

--- snippets/cd.js
+++ snippets/cd.js
@@ -1,3 +1,21 @@
 export function run(manager) {
-  console.log(manager);
+  const cd = 3000; // 3s
+  const map = Object.create(null);
+
+  manager.use({
+    name: 'cd',
+    willRender(ref) {
+      const now = Date.now();
+      const content = ref.danmaku.data;
+      const prevTime = map[content];
+
+      if (prevTime && now - prevTime < cd) {
+        ref.prevent = true;
+        console.warn(`"${content}" is blocked.`);
+      } else {
+        map[content] = now;
+      }
+      return ref;
+    },
+  });
 }
