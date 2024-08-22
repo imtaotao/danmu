90b1fe7e085c771a6716df1ff36eb1c5c148c9f4
imtaotao <chentao.arthur@bytedance.com>
2024-08-22 16:40:27 +0800

    release: v0.6.8-beta-1724316027386.0

--- release.ts
+++ release.ts
@@ -1,29 +1,42 @@
-import fs from 'node:fs';
-import semver from 'semver';
-import bumpPrompt from '@jsdevtools/version-bump-prompt';
-import { run, log, args } from './utils';
-
 // pnpm release --patch -> 1.0.1
 // pnpm release --minor -> 1.1.0
 // pnpm release --major -> 2.0.0
 // pnpm release --beta  -> 1.0.1-beta-1645598740512.0
+
+import fs from 'node:fs';
+import chalk from 'chalk';
+import semver from 'semver';
+import minimist from 'minimist';
+import bumpPrompt from '@jsdevtools/version-bump-prompt';
+
 type ReleaseType = 'prerelease' | 'patch' | 'minor' | 'major';
+
+const args = minimist(process.argv.slice(2));
 
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 
+const log = (msg: string) => console.log(chalk.cyan(msg));
+
 let type: ReleaseType = 'patch'; // default
 
-if (args.beta) {
-  type = 'prerelease';
-} else if (args.minor) {
-  type = 'minor';
-} else if (args.major) {
-  type = 'major';
+async function run(bin: string, args: Array<string>, opts = {}) {
+  const { execa } = await import('execa');
+  const result = await execa(bin, args, {
+    stdio: 'pipe',
+    cwd: process.cwd(),
+    ...opts,
+  });
+  if (result.exitCode) {
+    log(`${result.stdout}\n${result.stderr}\n`);
+    console.log(
+      chalk.red(`\nCommand execution failed, Code "${result.exitCode}"`),
+    );
+    process.exit(1);
+  }
+  return result;
 }
 
-release(type);
-
-export async function release(releaseType: ReleaseType) {
+async function release(releaseType: ReleaseType) {
   if (!pkg.version) return;
   log('\nBuilding...');
   await build();
@@ -37,7 +50,7 @@ export async function release(releaseType: ReleaseType) {
   await publish(data.newVersion);
 
   log('\nPush to GitHub...');
-  await run('git', ['push']);
+  await run('git', ['push', '--follow-tags']);
 
   log(`\nUpdated: "${data.oldVersion}" -> "${data.newVersion}"`);
 }
@@ -81,3 +94,13 @@ function bumpVersion(releaseType: ReleaseType) {
     files: ['./package.json'],
   });
 }
+
+if (args.beta) {
+  type = 'prerelease';
+} else if (args.minor) {
+  type = 'minor';
+} else if (args.major) {
+  type = 'major';
+}
+
+release(type);
