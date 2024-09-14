1e029b8f5ac1f7014e6a2ae7cff7840d6bd9c5f1
imtaotao <dwchentao@163.com>
2024-09-14 21:16:55 +0800

    fix: fix demo build error

--- demo/src/components/sidebar/SidebarNumbers.tsx
+++ demo/src/components/sidebar/SidebarNumbers.tsx
@@ -1,5 +1,4 @@
 import { memo, useState, useEffect } from 'react';
-import { sleep } from 'aidly';
 import { Asterisk } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import type { Manager } from 'danmu';
